239d7b1d4ea58e43068e77ce168e48e171123f5f
Jorge Tenorio <jordave34@gmail.com>
2011-12-13 00:28:04 -0500

    inicio del markup pantalla asientos

--- protected/modules/contabilidad/views/asiento/index.php
+++ protected/modules/contabilidad/views/asiento/index.php
@@ -2,6 +2,21 @@
 $this->breadcrumbs=array(
 	'Asiento',
 );?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
-
-<p>You may change the content of this page by modifying the file <tt><?php echo __FILE__; ?></tt>.</p>
+<h1>Asientos de Diario / Notas de cr&eacute;dito / Dep&oacute;sito</h1>
+<div>
+    <table>
+        <tr>
+            <td>Nº de asiento</td>
+            <td></td>
+            <td>Tipo Comprobante</td>
+            <td></td>
+            <td>Nº Comprobante</td>
+            <td></td>
+            <td>Ref</td>
+        </tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+    </table>
+</div>
